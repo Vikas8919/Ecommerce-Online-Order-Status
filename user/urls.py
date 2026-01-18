403489f9a65eaae5cc702e02894f5fa2c8b4734a
vikas2465 <vikas.lucky734@gmail.com>
2026-01-18 13:31:52 +0530

    User Registraion and Login Component Created

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth
+
+urlpatterns = [
+    path('', views.index, name ='index'),
+    path('login/', views.Login, name='login'),
+    path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'),
+    path('register/', views.register, name='register'),
+]
